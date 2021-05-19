ee0e6d282a89449393d6e72aaaf824bb508ce5ab
TedGu <80628029+TedGu@users.noreply.github.com>
2021-05-19 10:16:46 -0600

    Update useStyles.ts

--- client/src/pages/ReferFriend/useStyles.ts
+++ client/src/pages/ReferFriend/useStyles.ts
@@ -62,32 +62,4 @@ export const styles = (theme: Theme) =>
 
 const useStyles = makeStyles(styles);
 
-// const useStyles = makeStyles((theme) => ({
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(1),
-//   },
-
-//   inputs: {
-//     marginTop: '.8rem',
-//     height: '2rem',
-//     padding: '5px',
-//   },
-//   forgot: {
-//     paddingRight: 10,
-//     color: '#3a8dff',
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 2, 2),
-//     padding: 10,
-//     width: 160,
-//     height: 56,
-//     borderRadius: theme.shape.borderRadius,
-//     marginTop: 49,
-//     fontSize: 16,
-//     backgroundColor: '#3a8dff',
-//     fontWeight: 'bold',
-//   },
-// }));
-
 export default useStyles;
