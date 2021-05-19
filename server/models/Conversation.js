dcd403316c27b744bf25e2fccaa66a8295a82e61
lei.wang758 <lei.wang2@ucalgary.ca>
2021-05-19 10:32:40 -0600

    add conversation and message model #2

---
+++ server/models/Conversation.js
@@ -0,0 +1,29 @@
+const Message = require("./Message");
+const mongoose = require("mongoose");
+
+const conversationSchema = new mongoose.Schema({
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+
+  messages: [Message],
+
+  start_date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+
+  last_date: {
+    type: Date,
+  },
+});
+
+module.exports = Conversation = mongoose.model(
+  "conversion",
+  conversationSchema
+);
