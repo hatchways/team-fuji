50070f5179ef912d97da93ab8d8f1a0199c234d3
Stefan <stefanleoussis@gmail.com>
2021-05-20 15:27:25 -0400

    Created and Styled Demo Button
    Demo button logs in a user with hardcoded credentials

--- client/src/components/AuthHeader/useStyles.ts
+++ client/src/components/AuthHeader/useStyles.ts
@@ -30,6 +30,19 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: 'none',
     marginRight: 35,
   },
+  btnDivider: {
+    marginRight: '30px',
+  },
+  demoBtn: {
+    width: 170,
+    height: 54,
+    borderRadius: theme.shape.borderRadius,
+    filter: 'drop-shadow(0px 2px 6px rgba(74,106,149,0.2))',
+    backgroundColor: '	#C8C8C8',
+    color: 'white',
+    boxShadow: 'none',
+    marginRight: 35,
+  },
 }));
 
 export default useStyles;
