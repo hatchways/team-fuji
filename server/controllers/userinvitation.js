862a8352afd7ccd0ceacd5030a46ed725959472c
TedGu <oliveegu@gmail.com>
2021-05-27 00:25:15 -0600

    fix bugs for comments
    -merged routes with #8
    -added protect and validator

--- server/controllers/userinvitation.js
+++ server/controllers/userinvitation.js
@@ -6,7 +6,7 @@ const Invitation = require("../models/Invitation");
 // @desc Create invitation from specific user
 exports.createInvitation = asyncHandler(async (req, res, next) => {
   const { toUser, toUserEmail } = req.body;
-  try {    
+  try {
     const invitation = new Invitation({
       fromUser: req.params.id,
       toUser: toUser,
@@ -16,12 +16,16 @@ exports.createInvitation = asyncHandler(async (req, res, next) => {
     });
     const savedInvitationInfo = await invitation.save();
     res.json({
-        status: "success",
-        message: "Invitation created successfully",
-        data: savedInvitationInfo,
-      });
+      status: "success",
+      message: "Invitation created successfully",
+      data: savedInvitationInfo,
+    });
   } catch (error) {
-    res.json({ message: error });
+    console.log(new Date(), error);
+    res.json({
+      status: 500,
+      message: error,
+    });
   }
 });
 
@@ -33,33 +37,41 @@ exports.getPendingInvitation = asyncHandler(async (req, res, next) => {
     const pendingInvitations = await Invitation.find({
       toUser: userId,
       approved: false,
-      rejected: false
+      rejected: false,
     });
     res.json({
-        status: "success",
-        message: "Got pending invitations successfully",
-        data: pendingInvitations,
-      });
+      status: "success",
+      message: "Got pending invitations successfully",
+      data: pendingInvitations,
+    });
   } catch (error) {
-    res.json({ message: error });
+    console.log(new Date(), error);
+    res.json({
+      status: 500,
+      message: error,
+    });
   }
 });
 
 // @route GET /user/:id/contracts
 // @desc Get a list of contacts specific user accepted
-exports.getContracts = asyncHandler(async (req, res, next) => {
+exports.getContacts = asyncHandler(async (req, res, next) => {
   try {
     const userId = req.params.id;
     const acceptedInvitations = await Invitation.find({
       toUser: userId,
-      approved: true
+      approved: true,
     });
     res.json({
-        status: "success",
-        message: "Got accepted invitations successfully",
-        data: acceptedInvitations,
-      });
+      status: "success",
+      message: "Got accepted invitations successfully",
+      data: acceptedInvitations,
+    });
   } catch (error) {
-    res.json({ message: error });
+    console.log(new Date(), error);
+    res.json({
+      status: 500,
+      message: error,
+    });
   }
 });
