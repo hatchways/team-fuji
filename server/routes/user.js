f731ce4477dfdbe40465066a96ef5540dc3a9f13
lei.wang758 <lei.wang2@ucalgary.ca>
2021-06-01 11:06:19 -0600

    delete previous translation route #10

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,15 +7,11 @@ const {
   postUserConversation,
 } = require("../controllers/conversation");
 const { getMessages, postMessage } = require("../controllers/message");
-const { postMessageTranslation } = require("../controllers/translation");
 
 router.route("/").get(protect, searchUsers);
 router.route("/conversations").get(protect, getUserConversations);
 router.route("/conversation/:userId").post(protect, postUserConversation);
 router.route("/messages/:conversationId").get(protect, getMessages);
 router.route("/message/:conversationId").post(protect, postMessage);
-router
-  .route("/translation/:conversationId")
-  .post(protect, postMessageTranslation);
 
 module.exports = router;
