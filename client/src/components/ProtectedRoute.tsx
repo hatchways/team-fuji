aaf82d357fb14f9c29a725d02d626fd7bfce884e
lei.wang758 <lei.wang2@ucalgary.ca>
2021-06-03 13:29:07 -0600

    resolve warnings in protected route

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,30 +1,17 @@
-import React from 'react';
-import { Route, useHistory } from 'react-router-dom';
+import { Route, RouteProps, Redirect } from 'react-router-dom';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { useAuth } from '../context/useAuthContext';
 
-interface Props {
-  component: React.ComponentType<any>;
-  [x: string]: any;
-}
-
-const ProtectedRoute = ({ component: Component, ...rest }: Props): JSX.Element => {
+const ProtectedRoute = (routeProps: RouteProps): JSX.Element => {
   const { loggedInUser } = useAuth();
-  const history = useHistory();
 
   // render the page conditionally based on user being logged in
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (loggedInUser === undefined) return <CircularProgress />;
-        if (!loggedInUser) {
-          history.push('login');
-        }
-        return <Component {...rest} {...props} />;
-      }}
-    />
-  );
+  if (loggedInUser === undefined) return <CircularProgress />;
+  if (!loggedInUser) {
+    return <Redirect to={{ pathname: '/login' }} />;
+  } else {
+    return <Route {...routeProps} />;
+  }
 };
 
 export default ProtectedRoute;
