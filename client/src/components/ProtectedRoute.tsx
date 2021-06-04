ec545b0eca8e66bbd8775045b284b7117281333b
Stefan <stefanleoussis@gmail.com>
2021-06-04 10:27:35 -0400

    Revert "Suppresed Protected Route warnings for later update"
    This reverts commit eabca86c6c4c3b27fd07a4182543bffffe7e434e.

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -4,16 +4,14 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { useAuth } from '../context/useAuthContext';
 
 interface Props {
-  // eslint-disable-next-line
   component: React.ComponentType<any>;
-  // eslint-disable-next-line
   [x: string]: any;
 }
 
 const ProtectedRoute = ({ component: Component, ...rest }: Props): JSX.Element => {
   const { loggedInUser } = useAuth();
   const history = useHistory();
-  console.log(typeof Component);
+
   // render the page conditionally based on user being logged in
   return (
     <Route
